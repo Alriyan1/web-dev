3cb14f8c0f6d5a6d02d49f5bd8164758a4e37a14
Al Riyan Khan <alriyankhan7678@gmail.com>
2026-04-23 16:26:54 +0530

    till function

--- 03_Javascript/part1/script.js
+++ 03_Javascript/part1/script.js
@@ -11,3 +11,55 @@ u2 = Symbol('uid')
 
 // "5" - 1 = 4
 // 5 - "1" = 4
+
+switch(3){
+    case 1:
+        console.log('1')
+        break;
+    case 2:
+        console.log('2')
+        break
+    default:
+        console.log('default')
+}
+
+function temp(){
+    console.log('kkkkkkkkk')
+}
+
+let me = function(){
+    console.log('uuuuuuuu')
+} 
+
+let fn = ()=>{
+    console.log('iiiiiiii');
+}
+
+function ann(a=0,b=1){
+    console.log(a+b);
+}
+
+ann()
+
+function abcd(...val){
+    console.log(val);
+}
+
+
+abcd(1,2,3,4,5,6)
+
+(function(){
+    console.log('jjjjjjj');
+})();
+
+
+function getScore(...score){
+    let total = 0;
+    score.forEach(function (val){
+        total += val;
+    });
+    return total;
+}
+
+
+let e = getScore(1,2,3,4,5,6);
