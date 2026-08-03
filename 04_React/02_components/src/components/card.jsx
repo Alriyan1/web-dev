ad3bf94d3e93131afddad02554c488b59b86271c
Al Riyan <alriyankhan7678@gmail.com>
2026-08-03 18:16:04 +0530

    learn components

---
+++ 04_React/02_components/src/components/card.jsx
@@ -0,0 +1,14 @@
+const user = 'Alriyan';
+
+const card = () => {
+  return (
+    <div>
+        <div className="card">
+        <h1>{user}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, minima.</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
---
+++ 04_React/02_components/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+
+const navbar = () => {
+  return (
+    <div>
+        <div className="nav">
+            <h1>This is navbar</h1>
+        </div>
+    </div>
+  )
+}
+
+export default navbar
