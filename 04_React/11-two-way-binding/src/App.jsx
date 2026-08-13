4407132e248c66634bd784d1f699406621f377da
Al Riyan <alriyankhan7678@gmail.com>
2026-08-13 21:45:01 +0530

    learn 2-way binding

---
+++ 04_React/11-two-way-binding/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+const App = () => {
+
+  const [title,setTitle] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    // console.log(e.target[0].value)
+    console.log('Form Submitted by',title)
+    setTitle('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder="Enter Your Name" value={title} onChange={(e)=>{
+          setTitle(e.target.value)
+        }}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
