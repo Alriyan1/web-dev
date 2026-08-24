22980da2e6e8fcf30ce90df47732d3b24e20f6c8
Al Riyan <alriyankhan7678@gmail.com>
2026-08-24 13:21:37 +0530

    complete searchbar

--- 05_Redux/project/src/App.jsx
+++ 05_Redux/project/src/App.jsx
@@ -1,26 +1,9 @@
-import { fetchPhotos, fetchVideo } from "./api/MediaAPI";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
     <div className="h-screen w-full bg-gray-900 text-white">
-      <button
-        className="bg-green-400 px-4 py-2 m-5"
-        onClick={async () => {
-          const data = await fetchPhotos("cat");
-          console.log(data.results);
-        }}
-      >
-        Get Photos
-      </button>
-      <button
-        className="bg-green-400 px-4 py-2 m-5"
-        onClick={async () => {
-          const data = await fetchVideo("cat");
-          console.log(data.videos);
-        }}
-      >
-        Get Video
-      </button>
+      <SearchBar/>
     </div>
   );
 };
---
+++ 05_Redux/project/src/components/SearchBar.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useDispatch } from 'react-redux'
+import { setQuery } from "../redux/features/searchSlice";
+
+const SearchBar = () => {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch()
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    // console.log("form submitted");
+    dispatch(setQuery(text))
+    setText('')
+  };
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} className="flex bg-gray-800 gap-5 p-10">
+        <input
+          required
+          value={text}
+          onChange={(e)=>{
+            setText(e.target.value)
+          }}
+          className="w-full border-2 px-4 py-2 text-xl rounded outline-none"
+          type="text"
+          placeholder="Search anything..."
+        />
+        <button className="active:scale-95 cursor-pointer border-2 px-4 py-2 text-xl rounded outline-none">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
