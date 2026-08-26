50f990f7ecf26b738e83267161454de99f048d1e
Al Riyan <alriyankhan7678@gmail.com>
2026-08-26 22:04:18 +0530

    start learning typescript

---
+++ 06_Typescript/01_basic_implementation/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+var b = 13;
+let arr = [1, 2, 3, 4];
+//# sourceMappingURL=app.js.map
---
+++ 06_Typescript/01_basic_implementation/app.ts
@@ -0,0 +1,4 @@
+var a = 12;
+var b=13; 
+
+let arr: number[] =[1,2,3,4];
