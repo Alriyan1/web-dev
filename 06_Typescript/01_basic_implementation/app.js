09c62d3d41039aa58ddca52f660afdeb42568fd1
Al Riyan <alriyankhan7678@gmail.com>
2026-08-28 10:55:43 +0530

    add till class

--- 06_Typescript/01_basic_implementation/app.js
+++ 06_Typescript/01_basic_implementation/app.js
@@ -1,6 +1,27 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var a = 12;
 var b = 13;
 let arr = [1, 2, 3, 4];
+let arr2 = [12, 'alriyan'];
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super-admin";
+})(UserRoles || (UserRoles = {}));
+function getData(obj) {
+}
+function newVersion(obj) {
+    obj.admin = true;
+}
+let z;
+function abcd(a) {
+    a.getDetails;
+}
+class Device {
+    name = 'lg';
+    price = 12000;
+    category = 'digital';
+}
+let d1 = new Device();
+let d2 = new Device();
 //# sourceMappingURL=app.js.map
--- 06_Typescript/01_basic_implementation/app.ts
+++ 06_Typescript/01_basic_implementation/app.ts
@@ -2,3 +2,57 @@ var a = 12;
 var b=13; 
 
 let arr: number[] =[1,2,3,4];
+
+let arr2: [number,string] =[12,'alriyan']
+
+
+enum UserRoles {
+    ADMIN = 'admin',
+    GUEST = 'guest',
+    SUPER_ADMIN = 'super-admin'
+}
+
+interface User{
+    name: string,
+    email: string,
+    password: string,
+    gender?: string, //optional
+}
+
+function getData(obj:User){
+
+}
+
+interface Admin extends User{
+    admin:boolean;
+}
+
+function newVersion(obj:Admin){
+    obj.admin = true
+}
+
+type sankhy = number | string;
+
+let z: sankhy;
+
+type U = {
+    name:string,
+    email:string
+}
+
+type A = U & {
+    getDetails(user: string):void
+}
+
+function abcd(a:A){
+    a.getDetails
+  }
+
+class Device{
+    name='lg'
+    price=12000
+    category='digital'
+}
+
+let d1 = new Device()
+let d2 = new Device()
