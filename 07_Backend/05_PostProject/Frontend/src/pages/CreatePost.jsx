b1b676186f221052822970dbf60368a249c906b0
Al Riyan <alriyankhan7678@gmail.com>
2026-09-09 11:17:41 +0530

    little change in createpost page

--- 07_Backend/05_PostProject/Frontend/src/pages/CreatePost.jsx
+++ 07_Backend/05_PostProject/Frontend/src/pages/CreatePost.jsx
@@ -11,7 +11,7 @@ const CreatePost = () => {
         const formData = new FormData(e.target)
 
         axios.post('http://localhost:3000/create-post',formData)
-        .then((res)=>{
+        .then(()=>{
             navigate('/feed')
         })
         .catch((err)=>{
