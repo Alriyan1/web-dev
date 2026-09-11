cab391d58007d4562a805b70d54721dbf5603743
Al Riyan <alriyankhan7678@gmail.com>
2026-09-11 18:12:58 +0530

    add album feature

--- 07_Backend/07_Project2/src/controllers/music.controller.js
+++ 07_Backend/07_Project2/src/controllers/music.controller.js
@@ -1,6 +1,7 @@
 const musicModel = require("../models/music.model");
 const jwt = require('jsonwebtoken');
 const { uploadFile } = require("../services/storage.service");
+const albumModel = require("../models/album.model");
 
 
 async function createMusic(req,res) {
@@ -45,4 +46,41 @@ async function createMusic(req,res) {
 
 }
 
-module.exports = {createMusic}
+async function createAlbum(req,res) {
+    const token = req.cookies.token;
+
+    if (!token){
+        return res.status(401).json({message:'Unauthorized'})
+    }
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        if(decoded.role!=='artist'){
+            return res.status(403).json({message:"You don't have access to create album"})
+        }
+        
+        const {title,musics} = req.body;
+        const album = await albumModel.create({
+            title,
+            artist:decoded.id,
+            musics:musics
+        })
+
+        res.status(201).json({
+            message:"Album created successfully",
+            album: {
+                id: album._id,
+                title: album.title,
+                artist: album.artist,
+                musics: album.musics
+            }
+        })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(401).json({message:'Unauthorized'})
+    }
+}
+
+module.exports = {createMusic,createAlbum }
