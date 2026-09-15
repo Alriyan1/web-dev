a07faa00fb14435491679143ed9f32961138a38c
Al Riyan <alriyankhan7678@gmail.com>
2026-09-15 22:41:12 +0530

    complete backend

--- 07_Backend/08_Jest&ExpressValidator/src/app.js
+++ 07_Backend/08_Jest&ExpressValidator/src/app.js
@@ -1,10 +1,21 @@
-const express = require('express')
+const express = require('express');
+const  validationRules = require('./middlewares/validation.middleware');
 
 const app = express()
+app.use(express.json())
 
 
 app.get('/',(req,res)=>{
     res.status(200).json({message:"Hello, World!"})
+
+})
+
+app.post("/register",validationRules.registerUserValidationRules,(req,res)=>{
+    const {username,email,password} = req.body;
+
+    res.status(201).json({message:"User register successfully",user:{
+        username,email
+    }})
 })
 
 module.exports = app;
