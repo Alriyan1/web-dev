81d690636222b1ae09e5e548be0a89fac0e1544b
Al Riyan <alriyankhan7678@gmail.com>
2026-09-26 19:36:05 +0530

    working on transaction api

--- 08_Backend-Ledger/src/services/email.service.js
+++ 08_Backend-Ledger/src/services/email.service.js
@@ -50,6 +50,46 @@ async function sendRegistrationEmail(userEmail,name){
     await sendEmail(userEmail,subject,text,html);
 }
 
+async function sendTransactionEmail(userEmail,name,amount,toAccount){
+    const subject = 'Transaction Successfully';
+    const text = `Hello ${name},
+    Your transaction of ${amount} to ${toAccount} has been successfully completed.
+    Thank you for using our service.
+    Best regards,
+    Backend Ledger Team
+    `;
+    const html = `
+    <h1>Transaction Successfully</h1>
+    <p>Your transaction of ${amount} to ${toAccount} has been successfully completed.</p>
+    <p>Thank you for using our service.</p>
+    <p>Best regards,</p>
+    <p>Backend Ledger Team</p>
+    `;
+    await sendEmail(userEmail,subject,text,html);
+}
+
+async function sendTransactionFailureEmail(userEmail,name,amount,toAccount){
+    const subject = 'Transaction Failed';
+    const text = `Hello ${name},
+    Your transaction of ${amount} to ${toAccount} has failed.
+    Please try again.
+    Best regards,
+    Backend Ledger Team
+    `;
+
+    const html = `
+    <h1>Transaction Failed</h1>
+    <p>Your transaction of ${amount} to ${toAccount} has failed.</p>
+    <p>Please try again.</p>
+    <p>Best regards,</p>
+    <p>Backend Ledger Team</p>
+    `;
+    await sendEmail(userEmail,subject,text,html);
+}
+
+
 module.exports = {
     sendRegistrationEmail,
+    sendTransactionEmail,
+    sendTransactionFailureEmail,
 }
